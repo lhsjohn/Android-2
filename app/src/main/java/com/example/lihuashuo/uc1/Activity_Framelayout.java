8a2bfff554dd5384b304386f5c95e28eba11bb7f
lhsjohn <33946278+lhsjohn@users.noreply.github.com>
2018-04-19 11:21:51 +0800

    加入了帧布局的定时器切换效果

---
+++ app/src/main/java/com/example/lihuashuo/uc1/Activity_Framelayout.java
@@ -0,0 +1,70 @@
+package com.example.lihuashuo.uc1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by lihuashuo on 2018/4/19.
+ */
+
+public class Activity_Framelayout extends Activity {
+    private int currentImageView=0;
+    //定义一个图片数组
+    final int images[]=new int[]{
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g
+    };
+    final int names[]=new int[]{
+            R.id.btn1_framelayout,
+            R.id.btn2_framelayout,
+            R.id.btn3_framelayout,
+            R.id.btn4_framelayout,
+            R.id.btn5_framelayout
+    };
+    ImageView[] imageViews=new ImageView[names.length];
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+        //表明消息是由本程序发送的
+            if (msg.what==0x123){
+                for (int i=0;i<names.length;i++)
+                {
+                    imageViews[i].setBackgroundResource(images[(i+currentImageView)%names.length]);
+
+                }
+                currentImageView++;
+
+            }
+
+        }
+
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framelayout);
+        for (int i=0;i<names.length;i++){
+            imageViews[i]=findViewById(names[i]);
+        }
+        //定义一个线程周期性的改变currentImageView的值
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //发送一条空消息通知系统改变5个ImageView
+                handler.sendEmptyMessage(0x123);
+            }
+        },0,1000);
+
+    }
+}
